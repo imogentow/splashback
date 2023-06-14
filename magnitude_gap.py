e164b37bfab27069dccc2080c4488dfaf9003a32
Imogen Towler <imogentow@users.noreply.github.com>
2023-06-14 16:51:29 +0100

    Updated to work on new magnitude data format.

--- magnitude_gap.py
+++ magnitude_gap.py
@@ -2,14 +2,18 @@ import splashback as sp
 import numpy as np
 import matplotlib.pyplot as plt
 
-box = "L1000N1800"
+box = "L1000N3600"
 flm = sp.flamingo(box, "HF")
 flm.read_properties()
-# mass_mask = np.where(flm.M200m < 10**14.5)[0]
-magnitudes = np.genfromtxt(flm.path + "_galaxy_magnitudes.csv", delimiter=",")#[mass_mask]
-gap2 = magnitudes[:,1] - magnitudes[:,0] 
-gap4 = (magnitudes[:,2] - magnitudes[:,0])
-flm.gap = gap4
+# mass_mask = np.where(flm.M200m > 10**15)[0]
+magnitudes = np.genfromtxt(flm.path + "_galaxy_magnitudes.csv", delimiter=",")
+# flm.DM_density_3D = flm.DM_density_3D[mass_mask]
+# flm.gas_density_3D = flm.gas_density_3D[mass_mask]
+# flm.accretion = flm.accretion[mass_mask]
+sorted_magnitudes = np.sort(magnitudes)
+mag_bcg = sorted_magnitudes[:,0]
+mag_fourth = sorted_magnitudes[:,2]
+flm.gap = mag_fourth - mag_bcg
 
 N_bins = 5
 gap_bins = np.linspace(0, 3, N_bins+1)
@@ -34,19 +38,17 @@ plt.hist(flm.gap, bins=50, range=(0,4))
 plt.show()
 
 plt.figure()
-plt.scatter(gap4, magnitudes[:,0], color="r", s=2)
-plt.scatter(gap4, magnitudes[:,2], color="b", s=2)
+plt.scatter(flm.gap, magnitudes[:,1], color="r", s=2)
+plt.scatter(flm.gap, magnitudes[:,2], color="b", s=2)
 plt.xlabel("M14")
 plt.ylabel("r-band magnitude")
 plt.show()
 
-mask2 = np.where(np.isfinite(flm.accretion) & np.isfinite(gap2))[0]
-mask4 = np.where(np.isfinite(flm.accretion) & np.isfinite(gap4))[0]
+mask = np.where(np.isfinite(flm.accretion) & np.isfinite(flm.gap))[0]
 
-print(np.corrcoef(flm.accretion[mask2], gap2[mask2]))
-print(np.corrcoef(flm.accretion[mask4], gap4[mask4]))
+print(np.corrcoef(flm.accretion[mask], flm.gap[mask]))
 
-plt.scatter(flm.accretion, gap4)
+plt.scatter(flm.accretion, flm.gap)
 plt.xlabel("$\Gamma$")
 plt.ylabel("M14")
 plt.show()
