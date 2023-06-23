458a8ba8b62722da5144a2cb04c659c1c5e2e461
Imogen Towler <imogentow@users.noreply.github.com>
2023-06-23 11:03:49 +0100

    Compares the effect of the magnitude stacking at two different resolutions.

--- magnitude_gap.py
+++ magnitude_gap.py
@@ -2,7 +2,9 @@ import splashback as sp
 import numpy as np
 import matplotlib.pyplot as plt
 
-box = "L1000N3600"
+plt.style.use("mnras.mplstyle")
+
+box = "L1000N1800"
 flm = sp.flamingo(box, "HF")
 flm.read_properties()
 # mass_mask = np.where(flm.M200m > 10**15)[0]
@@ -15,9 +17,19 @@ mag_bcg = sorted_magnitudes[:,0]
 mag_fourth = sorted_magnitudes[:,2]
 flm.gap = mag_fourth - mag_bcg
 
+flm_high = sp.flamingo("L1000N3600", "HF")
+flm_high.read_properties()
+# mass_mask = np.where(flm.M200m > 10**15)[0]
+magnitudes = np.genfromtxt(flm_high.path + "_galaxy_magnitudes.csv", delimiter=",")
+sorted_magnitudes = np.sort(magnitudes)
+mag_bcg = sorted_magnitudes[:,0]
+mag_fourth = sorted_magnitudes[:,2]
+flm_high.gap = mag_fourth - mag_bcg
+
 N_bins = 5
 gap_bins = np.linspace(0, 3, N_bins+1)
 sp.stack_and_find_3D(flm, "gap", gap_bins)
+sp.stack_and_find_3D(flm_high, "gap", gap_bins)
 
 plt.figure()
 cm = plt.cm.copper(np.linspace(0,1,N_bins))
@@ -30,25 +42,47 @@ for i in range(N_bins):
 plt.legend()
 plt.xlabel("$r/R_{\\rm{200m}}$")
 plt.ylabel("$d \log \\rho_{\\rm{DM}} / d \log r$")
+filename = "splashback_data/flamingo/plots/magnitude_gap_profiles.png"
+plt.savefig(filename, dpi=300)
 plt.show()
 
-print(len(flm.gap[np.isfinite(flm.gap)]))
-
-plt.hist(flm.gap, bins=50, range=(0,4))
+fig, ax = plt.subplots(nrows=2, ncols=1,
+                       sharex = True, sharey=True,
+                       figsize=(3.321,4),
+                       gridspec_kw={'hspace' : 0, 'wspace' : 0})
+cm = plt.cm.copper(np.linspace(0,1,N_bins))
+for i in range(N_bins):
+    label = str(np.round(gap_bins[i],2)) \
+            + "$< \\rm{M14} <$" \
+            + str(np.round(gap_bins[i+1],2))
+    ax[0].semilogx(flm.rad_mid, flm.gap_log_DM[i,:], color=cm[i],
+                 linewidth=0.8, label=label)
+    ax[1].semilogx(flm_high.rad_mid, flm_high.gap_log_DM[i,:], color=cm[i],
+                 linewidth=0.8, label=label)
+ax[0].legend()
+ax[1].set_xlabel("$r/R_{\\rm{200m}}$")
+plt.ylabel("$d \log \\rho_{\\rm{DM}} / d \log r$")
+filename = "splashback_data/flamingo/plots/magnitude_gap_profiles_high_res.png"
+plt.savefig(filename, dpi=300)
 plt.show()
 
-plt.figure()
-plt.scatter(flm.gap, magnitudes[:,1], color="r", s=2)
-plt.scatter(flm.gap, magnitudes[:,2], color="b", s=2)
-plt.xlabel("M14")
-plt.ylabel("r-band magnitude")
-plt.show()
+# print(len(flm.gap[np.isfinite(flm.gap)]))
 
-mask = np.where(np.isfinite(flm.accretion) & np.isfinite(flm.gap))[0]
+# plt.hist(flm.gap, bins=50, range=(0,4))
+# plt.show()
 
-print(np.corrcoef(flm.accretion[mask], flm.gap[mask]))
+# plt.figure()
+# plt.scatter(flm.gap, magnitudes[:,1], color="r", s=2)
+# plt.scatter(flm.gap, magnitudes[:,2], color="b", s=2)
+# plt.xlabel("M14")
+# plt.ylabel("r-band magnitude")
+# plt.show()
 
-plt.scatter(flm.accretion, flm.gap)
-plt.xlabel("$\Gamma$")
-plt.ylabel("M14")
-plt.show()
+# mask = np.where(np.isfinite(flm.accretion) & np.isfinite(flm.gap))[0]
+
+# print(np.corrcoef(flm.accretion[mask], flm.gap[mask]))
+
+# plt.scatter(flm.accretion, flm.gap)
+# plt.xlabel("$\Gamma$")
+# plt.ylabel("M14")
+# plt.show()
