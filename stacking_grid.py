59b3bc50eef9dbbdfbdc986e75a0a15f0402bada
Imogen Towler <imogentow@users.noreply.github.com>
2023-07-20 16:54:36 +0100

    Now plots gas nad DM profiles side by side for better comparison.

--- stacking_grid.py
+++ stacking_grid.py
@@ -82,12 +82,12 @@ def stack_grid_bins(data, quantity1, quantity2):
     data.depth_gas = depth_gas
     
 
-def grid_plot(data, quantity, accretion_bins):
+def grid_plot_DM(data, quantity, accretion_bins):
     bins = getattr(data, quantity+"_bins")
     N_acc = len(accretion_bins) - 1
     N_bins = len(bins) - 1
     fig, ax = plt.subplots(nrows=N_acc, ncols=1,
-                           figsize=(3,6),
+                           figsize=(3.321,7),
                            sharex=True, sharey=True,
                            gridspec_kw={'hspace' : 0, 'wspace' : 0})
     cm = plt.cm.copper(np.linspace(0,1,N_bins))
@@ -110,7 +110,7 @@ def grid_plot(data, quantity, accretion_bins):
                         + r"$< \Gamma <$" \
                         + str(np.round(accretion_bins[i+1],2))
         ax[i].text(0.05, 0.05, plot_label, transform=ax[i].transAxes)
-    fig.text(0.02, 0.45, r"$d \log \rho_{\rm{DM}} / d \log r$",
+    fig.text(0.02, 0.45, r"$d \log \rho_{\rm{gas}} / d \log r$",
              transform=fig.transFigure, rotation='vertical')
     plt.xlabel("$r/R_{\\rm{200m}}$")
     ax[0].legend()
@@ -119,37 +119,80 @@ def grid_plot(data, quantity, accretion_bins):
     # plt.savefig(filename, dpi=300)
     plt.show()
     
+    
+def grid_plot_both(data, quantity, accretion_bins):
+    bins = getattr(data, quantity+"_bins")
+    N_acc = len(accretion_bins) - 1
+    N_bins = len(bins) - 1
+    fig, ax = plt.subplots(nrows=N_acc, ncols=2,
+                           figsize=(4,6),
+                           sharex=True, sharey=True,
+                           gridspec_kw={'hspace' : 0, 'wspace' : 0})
+    cm = plt.cm.copper(np.linspace(0,1,N_bins))
+    symbol = labels[quantity]
+    for i in range(N_acc):
+        for j in range(N_bins):
+            # if j == N_mass-1:
+            #     label = r"$M_{\rm{200m}} >$" + str(np.round(mass_bins[j],2))
+            # else:
+            label = str(np.round(bins[j],2)) \
+            + r"$<$" + symbol + "$<$" \
+            + str(np.round(bins[j+1],2))
+            ax[i,0].semilogx(rad_mid, data.log_DM[j,i,:], 
+                             color=cm[j], linewidth=0.8,
+                             label=label)
+            ax[i,1].semilogx(rad_mid, data.log_gas[j,i,:], 
+                             color=cm[j], linewidth=0.8,
+                             label=label)
+        # if i == N_acc-1:
+        #     plot_label = r"$\Gamma >$" + str(np.round(accretion_bins[i],2))
+        # else:
+        plot_label = str(np.round(accretion_bins[i],2)) \
+                        + r"$< \Gamma <$" \
+                        + str(np.round(accretion_bins[i+1],2))
+        ax[i,0].text(0.05, 0.05, plot_label, transform=ax[i,0].transAxes)
+    fig.text(0.02, 0.45, r"$d \log \rho_{\rm{gas}} / d \log r$",
+             transform=fig.transFigure, rotation='vertical')
+    fig.text(0.45, 0.02, "$r/R_{\\rm{200m}}$",
+             transform=fig.transFigure,)
+    ax[0,0].legend()
+    ax[0,0].set_ylim((-4.5, 0.5))
+    plt.subplots_adjust(left=0.11, bottom=0.08)
+    filename = "splashback_data/flamingo/plots/grid_stacking_gas_DM.png"
+    plt.savefig(filename, dpi=300)
+    plt.show()
+    
 
 def stack_for_profiles(data):
     N_bins = 5
-    data.mass_bins = np.linspace(14, 15.2, N_bins)
+    data.mass_bins = np.linspace(14, 15, N_bins)
     data.accretion_bins = np.linspace(0, 4, N_bins)
-    data.concentration_bins = np.linspace(0.0, 0.4, N_bins)
-    data.symmetry_bins = np.linspace(0.0, 1.5, N_bins) 
-    data.alignment_bins = np.linspace(0.4, 1.6, N_bins) 
-    data.centroid_bins = np.linspace(-3, -1, N_bins)
+    # data.concentration_bins = np.linspace(0.0, 0.4, N_bins)
+    # data.symmetry_bins = np.linspace(0.0, 1.5, N_bins) 
+    # data.alignment_bins = np.linspace(0.4, 1.6, N_bins) 
+    # data.centroid_bins = np.linspace(-3, -1, N_bins)
  
     stack_grid_bins(data, "mass", "accretion")
-    grid_plot(data, "mass", data.accretion_bins)
+    grid_plot_both(data, "mass", data.accretion_bins)
     
-    data.DM_density_3D = np.vstack((data.DM_density_3D, 
-                                    data.DM_density_3D, 
-                                    data.DM_density_3D))
-    data.gas_density_3D = np.vstack((data.gas_density_3D, 
-                                     data.gas_density_3D, 
-                                     data.gas_density_3D))
+    # data.DM_density_3D = np.vstack((data.DM_density_3D, 
+    #                                 data.DM_density_3D, 
+    #                                 data.DM_density_3D))
+    # data.gas_density_3D = np.vstack((data.gas_density_3D, 
+    #                                  data.gas_density_3D, 
+    #                                  data.gas_density_3D))
     
-    stack_grid_bins(data, "concentration", "accretion")
-    grid_plot(data, "concentration", data.accretion_bins)
+    # stack_grid_bins(data, "concentration", "accretion")
+    # grid_plot(data, "concentration", data.accretion_bins)
     
-    stack_grid_bins(data, "symmetry", "accretion")
-    grid_plot(data, "symmetry", data.accretion_bins)
+    # stack_grid_bins(data, "symmetry", "accretion")
+    # grid_plot(data, "symmetry", data.accretion_bins)
     
-    stack_grid_bins(data, "alignment", "accretion")
-    grid_plot(data, "alignment", data.accretion_bins)
+    # stack_grid_bins(data, "alignment", "accretion")
+    # grid_plot(data, "alignment", data.accretion_bins)
     
-    stack_grid_bins(data, "centroid", "accretion")
-    grid_plot(data, "centroid", data.accretion_bins)
+    # stack_grid_bins(data, "centroid", "accretion")
+    # grid_plot(data, "centroid", data.accretion_bins)
     
     # plt.figure()
     # for i in range(N_bins):
@@ -159,7 +202,7 @@ def stack_for_profiles(data):
 
 
 if __name__ == "__main__":
-    box = "L1000N1800"
+    box = "L2800N5040"
     
     N_rad = 44
     log_radii = np.linspace(-1, 0.7, N_rad+1)
@@ -167,7 +210,8 @@ if __name__ == "__main__":
     
     flm_HF = sp.flamingo(box, "HF")
     flm_HF.read_properties()
-    flm_HF.read_2D_properties()
+    # flm_HF.read_low_mass()
+    # flm_HF.read_2D_properties()
     # flm_HF.read_low_mass()
     
     stack_for_profiles(flm_HF)
