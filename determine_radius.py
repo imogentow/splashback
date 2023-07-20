d30c2afba182b0236267a89e55658c1bd75414ca
Imogen Towler <imogentow@users.noreply.github.com>
2023-07-20 16:52:49 +0100

    Added function to identify maxima features.

--- determine_radius.py
+++ determine_radius.py
@@ -253,3 +253,96 @@ def depth_cut(radii, array,
     elif depth_value != "n" and second_caustic != "n":
         return R_sp, secondary, depth, second_depth
     return R_sp
+
+def find_maxima(radii, array, 
+                cut=-3,
+                plot="n"):
+    """Calculating Rsp by interpolating profiles.
+    radii   - corresponding radii data for log gradient profiles
+    density - array of gradient of log density values for either one cluster
+              or a group of clusters but must be size (N_clusters, N_rad_bins)
+    """
+    
+    if array.ndim != 1:
+        N = array.shape[0]
+    else:
+        N = 1
+        array = array[np.newaxis,:]
+        
+    R_sp = np.zeros(N)
+    depth = np.zeros(N)
+        
+    rad_range = radii[[9,-9]] #upper and lower bound set by window size
+    
+    for i in range(N):
+        if np.isnan(array[i,:]).all() == True:
+            R_sp[i] = np.nan
+            depth[i] = np.nan
+            continue
+        density = array[i,:]
+        finite = np.isfinite(density)
+        density = density[finite] #delete bad values in profiles
+        temp_radii = radii[finite]
+            
+        density_interp = interp1d(temp_radii, density, kind="cubic")
+        x_interp = 10**np.linspace(np.log10(temp_radii[1]), np.log10(temp_radii[-2]), 1000)
+        y_interp = density_interp(x_interp)
+            
+        x_min = np.where(x_interp > rad_range[0])[0][0]
+        x_max = np.where(x_interp < rad_range[1])[0][-1]
+        if x_interp[x_max] > 2.4:
+            x_max = np.where(x_interp > 2.4)[0][0]
+              
+        maxima = argrelextrema(y_interp, np.greater)[0]
+        depth_mask = np.where(y_interp[maxima] >= cut)[0]
+        maxima = maxima[depth_mask]
+                        
+        test_SP = np.argmax(y_interp)
+        maxima_range = np.arange(x_min, x_max, 1)
+                        
+        #Findsplashback
+        k = 0
+        l = 0
+        if len(maxima) == 0:
+            R_sp[i] = np.nan
+            depth[i] = np.nan
+            continue
+        #where to save minima values found to be in range
+        maxima_in_range = np.array([], dtype=int)
+        for j in maxima:
+            if j in maxima_range:
+                test_SP = j
+                maxima_in_range = np.append(maxima_in_range, j)
+                l += 1
+            else:
+                k += 1
+                                
+        if l > 1:
+            #if there are more than one minima in range, take the deepest
+            deepest = np.argmax(y_interp[maxima])
+            test_SP = maxima[deepest]
+                                
+        elif k == len(maxima):
+            #if there are no minima in range, take the deepest overall
+            deepest = np.argmax(y_interp[maxima])
+            test_SP = maxima[deepest]
+                
+        R_sp[i] = x_interp[test_SP]
+        depth[i] = y_interp[test_SP]
+                    
+        if plot != "n":   
+            # Looks at minima in distribution to check Rsp is chosen properly
+            plt.figure()
+            plt.semilogx(x_interp, y_interp, color="k")
+            plt.scatter(x_interp[maxima], y_interp[maxima], 
+                        marker="o", 
+                        color="r")
+            plt.scatter(x_interp[test_SP], y_interp[test_SP], 
+                        marker="*", 
+                        color="gold", 
+                        s=100)
+            plt.xlabel("r/$R_{200m}$")
+            plt.ylabel("$\mathrm{d} \log \\rho / \mathrm{d} \log r$")
+            plt.title(plot)
+            plt.show()
+    return R_sp
