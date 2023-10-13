c27d0edea543dff7968bbd01cf3e8d28270e38ef
Imogen Towler <imogentow@users.noreply.github.com>
2023-10-13 11:44:32 +0100

    Tidied code and added relevant docstrings.

--- stacking_hydro_runs.py
+++ stacking_hydro_runs.py
@@ -1,35 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import splashback as sp
-import determine_radius as dr
 
 plt.style.use("mnras.mplstyle")
 
-box = "L1000N1800"    
 axes_labels = {
     "mass": "$M_{\\rm{200m}}$",
     "accretion": "$\Gamma$",
-    "energy": "$E_{\\rm{kin}}/E_{\\rm{therm}}$"}
+    "energy": "$X_{\\rm{E}}$"}
+        
 
-def bin_profiles(d, accretion_bins, mass_bins, energy_bins, bootstrap="none"):
+def bin_profiles(data, accretion_bins, mass_bins, energy_bins, bootstrap="none"):
     """
     Takes a given run object and bins the density profiles according to
-    3 given bin arrays. Also calculates a best fit gradient, assuming that 
-    the line goes through the origin.
+    given bin arrays. 
     
-    Inputs.
-    d: obj, run of choice
-    accretion_bins: array giving edge values of bins of accretion rate
-    mass_bins: array giving edge values of bins of mass
-    energy_bins: array giving edge values of bins of energy ratio.
-    """
+    Parameters
+    ----------
+    data : obj
+        Simulation dataset object of choice
+    accretion_bins : numpy array
+        Array of bin edges used for stacking following the accretion rate.
+    mass_bins : numpy array
+        Array of bin edges used for stacking following the cluster mass.
+    energy_bins : numpy array
+        Array of bin edges used for stacking following the cluster energy 
+        ratio.
 
-    sp.stack_and_find_3D(d, "accretion", accretion_bins, bootstrap=bootstrap)
-    sp.stack_and_find_3D(d, "mass", mass_bins, bootstrap=bootstrap)
-    sp.stack_and_find_3D(d, "energy", energy_bins, bootstrap=bootstrap)
+    Returns
+    -------
+    None.
+
+    """
+    sp.stack_and_find_3D(data, "accretion", accretion_bins, bootstrap=bootstrap)
+    sp.stack_and_find_3D(data, "mass", mass_bins, bootstrap=bootstrap)
+    sp.stack_and_find_3D(data, "energy", energy_bins, bootstrap=bootstrap)
 
     
 def second_caustic(flm, split):
+    """
+    Sorts out which minima in a profile is the splashback radius and which is
+    the second caustic, replaces these values correctly.
+
+    Parameters
+    ----------
+    flm : obj
+        Simulation data.
+    split : str
+        Name of the original criteria used to stack the original profiles.
+
+    Returns
+    -------
+    None.
+
+    """
     second_all = getattr(flm, "2_DM_"+split)
     R_sp = getattr(flm, "R_DM_"+split)
     
@@ -43,52 +67,35 @@ def second_caustic(flm, split):
             second_all[index] = smaller
     setattr(flm, "R_DM_"+split, R_sp)
     setattr(flm, "2_DM_"+split, second_all)
-    
-    
-def plot_Rsp_scatter(list_of_sims, mids):
-    xlim = np.array([0.7,1.4])
-    ylim = (0.91,1.2)
-    bin_type = np.array(["accretion", "mass", "energy"])
-    labels = np.array(["$\Gamma$", "$\log M_{\\rm{200m}}$", "$E_{\\rm{kin}} / E_{\\rm{therm}}$"])
-    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(3.3,7),
-                            gridspec_kw={'hspace' : 0, 'wspace' : 0})
-                            # sharex=True, sharey=True)
-    cm = plt.cm.get_cmap('rainbow')
-    axes[0].plot(xlim, xlim, color="k")
-    axes[1].plot(xlim, xlim, color="k")
-    axes[2].plot(xlim, xlim, color="k")
-    N_runs = len(list_of_sims)
-    axes[0].set_xlim(xlim)
-    axes[0].set_ylim(ylim)
-    axes[1].set_xlim(xlim)
-    axes[1].set_ylim(ylim)
-    axes[2].set_xlim(xlim)
-    axes[2].set_ylim(ylim)
-    for i in range(3):
-        for k in range(N_runs):
-            crange = axes[i].scatter(getattr(list_of_sims[k], "R_DM_" + bin_type[i]),
-                                     getattr(list_of_sims[k], "R_gas_"+ bin_type[i]),
-                                     c=mids[i,:], edgecolor="k", 
-                                     cmap=cm, s=75, marker="o",
-                                     label=getattr(list_of_sims[k], "run_label"))
-        cbaxes = fig.add_axes([0.185, 0.21, 0.002, 0.005]) 
-        cbar = fig.colorbar(crange, cax=cbaxes, label=labels[i])
-        #colour bars don't quite work but don't need this plot anymore
-
-    axes[0].legend(loc='upper left')
-    axes[1].legend(loc='upper left')
-    axes[2].legend(loc='upper left')
-    axes[0].set_xticklabels([])
-    axes[1].set_xticklabels([])
-    axes[2].set_xlabel(r"$R_{\rm{SP,DM}} / R_{\rm{200m}}$")
-    axes[1].set_ylabel(r"$R_{\rm{SP,gas}} / R_{\rm{200m}}$")
-    # filename = "splashback_data/flamingo/plots/compare_runs_new.png"
-    # plt.savefig(filename, dpi=300)
-    plt.show()
 
 
 def plot_param_correlations(list_of_sims, ax, split, mids, 
                             ls, quantity="R_DM_"):
+    """
+    Plots parameter dependence onto one given panel of a figure.
+
+    Parameters
+    ----------
+    list_of_sims : list
+        List of simulation data objects.
+    ax : obj
+        Pyplot axes object.
+    split : str
+        Name of crtieria used to stack profiles that will be plotted on the 
+        x-axis.
+    mids : float
+        Midpoints of bins for x-axis.
+    ls : TYPE
+        Linestyle for plotting.
+    quantity : str, optional
+        Name of quantity to plot values of. Options are "R_DM_", "depth_DM_",
+        "R_gas_" and "depth_gas_". The default is "R_DM_".
+
+    Returns
+    -------
+    None.
+
+    """
     N_runs = len(list_of_sims)
     lw = 1
     if quantity == "R_DM_" or quantity == "depth_DM_":
@@ -120,162 +127,24 @@ def plot_param_correlations(list_of_sims, ax, split, mids,
         ax.set_xlabel(axes_labels[split])
 
     
-def plot_params_all(bins, y, labels):
+def stack_for_params(list_of_sims):
     """
-    Plots chosen quantity against bin mid points.
-    Could plot Rsp or gamma, should work for all.
+    Takes a variety of simulation runs and plots the dependence of 
+    both the radius and depth of the splashback feature on the criteria used 
+    to stack the profiles.
 
     Parameters
     ----------
-    bins : numpy array (3, N_bins)
-        Bin midpoints.
-    y : numpy array (N_runs, N_bins)
-        Quantity to plot on y-axis.
-    labels : list of strings
-        List of labels.
-    """
-
-    N_runs = y.shape[0]
-    
-    fig, ax = plt.subplots(nrows=1, ncols=3,
-                           sharey=True,
-                           figsize=(6,3))
-    cm = plt.cm.rainbow(np.linspace(0,1,N_runs))
-    for i in range(N_runs):
-        ax[0].scatter(bins[0,:], y[i,:,0], edgecolor="k", c=cm[i], label=labels[i])
-        ax[1].scatter(bins[1,:], y[i,:,1], edgecolor="k", c=cm[i])
-        ax[2].scatter(bins[2,:], y[i,:,2], edgecolor="k", c=cm[i])
-    ax[0].legend()
-    ax[0].set_ylabel("$R_{\\rm{SP}}$")
-    ax[0].set_xlabel("$\Gamma$")
-    ax[1].set_xlabel("$M_{\\rm{200m}}$")
-    ax[2].set_xlabel("$E_{\\rm{kin}} / E_{\\rm{therm}}$")
-    plt.show()
-    
-    
-def plot_all_profiles(list_of_sims, bins, quantity="DM"):
-    """
-    Compares all profiles for three different runs and three standard stacking
-    criteria.
-
-    Parameters
-    ----------
-    list_of_sims: list
-        List of objects, one for each simulation run wanted to plot. Order of
-        runs will be order of plotting.
-    bins : numpy array
-        Bins used to stack profiles for all three stacking criteria. 
-        (3,N_bins) In order: accretion rate, mass, energy ratio.
-    quantity : str, optional
-        Plot either gas or DM profiles. The default is "DM".
+    list_of_sims : list
+        List of simulation objects.
 
     Returns
     -------
     None.
 
     """
-    
-    bin_type = np.array(["accretion", "mass", "energy"])
-    labels = np.array(["$\Gamma$", "$\log M_{\\rm{200m}}$", "$E_{\\rm{kin}} / E_{\\rm{therm}}$"])
-    N_bins = bins.shape[1] -1
-    N_runs = len(list_of_sims)
-    if quantity == "DM":
-        ylim = (-4.6,0.8)
-        location = "upper left"
-    else:
-        ylim = (-3.9,2.1)
-        location = "upper center"
-    fig, ax = plt.subplots(nrows=N_runs, ncols=3, 
-                           figsize=(5.5,N_runs*1.8),
-                           sharex=True, sharey='row',
-                           gridspec_kw={'hspace' : 0, 'wspace' : 0})
-    cm1 = plt.cm.autumn(np.linspace(0,0.95,N_bins))
-    cm2 = plt.cm.winter(np.linspace(0,1,N_bins))
-    cm3 = plt.cm.copper(np.linspace(0,1,N_bins))
-    lw = 0.8
-    for j in range(3):
-        for i in range(0, N_bins):
-            if i == 0:
-                label = labels[j] + "$<$"  + str(np.round(bins[j,1],2))
-            elif i == N_bins-1:
-                label = labels[j] + "$>$" + str(np.round(bins[j,i],2))
-            else:
-                label = str(np.round(bins[j,i],2)) \
-                    + "$<$" + labels[j] + "$<$" \
-                    + str(np.round(bins[j,i+1],2))
-            if j == 0:
-                cm = cm1
-            elif j==1:
-                cm = cm2
-            else:
-                cm = cm3
-            for k in range(N_runs):
-                ax[k,j].semilogx(rad_mid, getattr(list_of_sims[k], bin_type[j] + "_log_" + quantity)[i,:], 
-                                 color=cm[i], linewidth=lw, label=label)
-                ax[k,0].text(0.05, 0.05, list_of_sims[k].run_label, transform=ax[k,0].transAxes)
-        ax[0,j].legend(loc=location)
-    for axes in ax.flatten():
-        axes.set_ylim(ylim)
-    
-    ax[-1,1].set_xlabel("$r/R_{\\rm{200m}}$")
-    #ax[int(N_runs/2),0].set_ylabel(r"$d \log \rho_{{\rm{{{}}}}} / d \log r$".format(quantity))
-    fig.text(0.02, 0.45, r"$d \log \rho_{{\rm{{{}}}}} / d \log r$".format(quantity),
-             transform=fig.transFigure, rotation='vertical')
-    plt.subplots_adjust(left=0.08)
-    filename = "splashback_data/flamingo/plots/HF_compare_all_" + quantity + ".png"
-    # plt.savefig(filename, dpi=300)
-    plt.show()
-    
-def stack_for_profiles(list_of_sims):
-    N_bins = 4
-    mass_bins = np.linspace(14, 15, N_bins+1)
-    mass_bins = np.append(mass_bins, 16)
-    accretion_bins = np.linspace(0, 4, N_bins+1)
-    accretion_bins = np.append(accretion_bins, 20)
-    energy_bins = np.linspace(0.05, 0.3, N_bins+1)
-    energy_bins = np.append(energy_bins, 1)
-    
-    N_runs = len(list_of_sims)
-    for i in range(N_runs):
-        bin_profiles(list_of_sims[i], accretion_bins, mass_bins, energy_bins)
-    bins = np.vstack((accretion_bins, mass_bins, energy_bins))
-    plot_all_profiles(list_of_sims, bins, quantity="DM")
-    plot_all_profiles(list_of_sims, bins, quantity="gas")
-    
-    
-def stack_for_Rsp(list_of_sims):
     N_bins = 10
-    mass_bins = np.linspace(14, 15, N_bins+1)
-    mass_bins = np.append(mass_bins, 16)
-    accretion_bins = np.linspace(0, 4, N_bins+1)
-    accretion_bins = np.append(accretion_bins, 20)
-    energy_bins = np.linspace(0.05, 0.3, N_bins+1)
-    energy_bins = np.append(energy_bins, 1)
-    
-    N_runs = len(list_of_sims)
-    for i in range(N_runs):
-        bin_profiles(list_of_sims[i], accretion_bins, mass_bins, energy_bins)
-    
-    mass_mid = np.zeros(N_bins+1)
-    accretion_mid = np.zeros(N_bins+1)
-    energy_mid = np.zeros(N_bins+1)
-
-    mass_mid[:-1] = (mass_bins[:-2] + mass_bins[1:-1])/2
-    accretion_mid[:-1] = (accretion_bins[:-2] + accretion_bins[1:-1])/2
-    energy_mid[:-1] = (energy_bins[:-2] + energy_bins[1:-1])/2
-    mass_mid[-1] = mass_bins[-2]
-    accretion_mid[-1] = accretion_bins[-2]
-    energy_mid[-1] = energy_bins[-2]
-
-    mids = np.vstack((accretion_mid, mass_mid, energy_mid))
-    
-    # Make big plot comparing Rsp for different runs and stacking criteria.
-    plot_Rsp_scatter(list_of_sims, mids)
-    
-    
-def stack_for_params(list_of_sims):
-    N_bins = 10
-    mass_bins = np.linspace(14, 15.2, N_bins+1)
+    mass_bins = np.linspace(14, 14.1, N_bins+1)
     accretion_bins = np.linspace(0, 4.2, N_bins+1)
     energy_bins = np.linspace(0.05, 0.35, N_bins+1)
     mass_mid = 10**((mass_bins[:-1] + mass_bins[1:])/2)
@@ -321,7 +190,7 @@ def stack_for_params(list_of_sims):
     axes[1,1].legend(ncol=2)
     axes[0,0].set_ylim((0.75, 1.2))
     axes[1,0].set_ylim((0.75, 1.2))
-    # filename = "splashback_data/flamingo/plots/parameter_dependence_cosmo_Rsp.png"
+    # filename = "splashback_data/flamingo/plots/parameter_dependence_all_runs_Rsp.png"
     # plt.savefig(filename, dpi=300)
     plt.show()
     
@@ -353,16 +222,14 @@ def stack_for_params(list_of_sims):
     axes[1,1].legend(ncol=2)
     axes[0,0].set_ylim((0.75, 1.2))
     axes[1,0].set_ylim((0.75, 1.2))
-    # filename = "splashback_data/flamingo/plots/parameter_dependence_cosmo_gamma.png"
+    # filename = "splashback_data/flamingo/plots/parameter_dependence_all_runs_gamma.png"
     # plt.savefig(filename, dpi=300)
     plt.show()
     
-    
-N_rad = 44
-log_radii = np.linspace(-1, 0.7, N_rad+1)
-rad_mid = (10**log_radii[1:] + 10**log_radii[:-1]) / 2
 
 if __name__ == "__main__":
+    box = "L1000N1800"   
+    
     hf = sp.flamingo(box, "HF")
     hf.read_properties()
 
@@ -392,9 +259,6 @@ if __name__ == "__main__":
     
     hpv = sp.flamingo(box, "HPV")
     hpv.read_properties()
-    
-    # stack_for_profiles([hf, hwa, hsa, hta, hj, hsj])
-    # stack_for_profiles(hp, hpf, hpv)
 
-    # stack_for_params([hf, hwa, hsa, hta, hua, hj, hsj])
-    stack_for_params([hf, hp, hpf, hpv])
+    stack_for_params([hf, hwa, hsa, hta, hua, hj, hsj])
+    # stack_for_params([hf, hp, hpf, hpv])
