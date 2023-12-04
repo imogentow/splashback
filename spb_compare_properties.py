b69b7a321ecfa322c3bca6f52f677abd50276bdf
Imogen Towler <imogentow@users.noreply.github.com>
2023-12-04 15:49:30 +0000

    Slight alterations to figure appearance.

--- spb_compare_properties.py
+++ spb_compare_properties.py
@@ -20,7 +20,7 @@ flm.symmetry = flm.symmetry[:N_clusters]
 flm.alignment = flm.alignment[:N_clusters]
 flm.centroid = flm.centroid[:N_clusters]
 
-properties_all = np.vstack((np.log10(flm.M200m), flm.accretion, flm.energy, flm.gap,
+properties_all = np.vstack((flm.accretion, np.log10(flm.M200m), flm.energy, flm.gap,
                        flm.concentration, flm.symmetry, flm.alignment, flm.centroid))
 list_good1 = np.intersect1d(
     np.intersect1d(np.intersect1d(np.where(np.isfinite(properties_all[0,:]))[0], 
@@ -34,18 +34,20 @@ list_good2 = np.intersect1d(
 list_good = np.intersect1d(list_good1, list_good2)
 
 plot_data = properties_all[:,list_good].T
-labels = [r'$\log \left( M_{\rm{200m}}/M_{\odot}\right)$', r'$\Gamma$', r'$X_{\rm{E}}$',
+labels = [r'$\Gamma$', r'$\log \left( M_{\rm{200m}}/M_{\odot}\right)$', r'$X_{\rm{E}}$',
+            '$M14$', '$c$', '$s$', '$a$', '$\log \langle w \\rangle$']
+labels_p = [r'$\Gamma$', r'$M_{\rm{200m}}$', r'$X_{\rm{E}}$',
             '$M14$', '$c$', '$s$', '$a$', '$\log \langle w \\rangle$']
 N_properties = len(labels)
 
-fig, ax = plt.subplots(N_properties, N_properties, figsize=(6,6), dpi=300,
+fig, ax = plt.subplots(N_properties, N_properties, figsize=(6.5,6), dpi=300,
                        gridspec_kw={'hspace' : 0, 'wspace' : 0})
 fig.set_facecolor('w')
 fig= corner.corner(
     plot_data,
     fig=fig,
     plot_contours=True, plot_density=False, plot_datapoints=True,
-    labels=labels,
+    #labels=labels,
     data_kwargs=dict(alpha=0.8, color='cornflowerblue'),
     hist_kwargs=dict(linewidth=0.8, color='cornflowerblue', histtype='stepfilled', density=True),
     hist2d_kwargs=dict(linewidth=0.8),
@@ -62,7 +64,10 @@ fig= corner.corner(
 for i in range(N_properties):
     ax[i, i].yaxis.set_label_position('right')
     ax[i, i].yaxis.set_ticks_position('right')
-    ax[i, i].set_ylabel(r'$P(${}$)$'.format(labels[i]))
+    ax[i, i].set_ylabel(r'$P(${}$)$'.format(labels_p[i]))
+    if i >= 1:
+        ax[i,0].set_ylabel(labels[i], labelpad=5)
+    ax[N_properties-1,i].set_xlabel(labels[i], labelpad=5)
 
 M_lim = (14,15.55)
 Gamma_lim = (0,8)
@@ -74,10 +79,10 @@ a_lim = (0,2)
 w_lim = (-3, -1)
 
 for i in range(N_properties):
-    ax[i,0].set_xlim(M_lim)
+    ax[i,0].set_xlim(Gamma_lim)
     
 for i in range(N_properties-1):
-    ax[i+1,1].set_xlim(Gamma_lim)
+    ax[i+1,1].set_xlim(M_lim)
     ax[N_properties-1,i].set_ylim(w_lim)
 
 for i in range(N_properties-2):
@@ -101,7 +106,7 @@ ax[7,6].set_xlim(a_lim)
 
 ax[7,7].set_xlim(w_lim)
 
-ax[1,0].set_ylim(Gamma_lim)
+ax[1,0].set_ylim(M_lim)
 
 ax[2,0].set_ylim(E_lim)
 ax[2,1].set_ylim(E_lim)
@@ -110,7 +115,7 @@ ax[3,0].set_ylim(mag_lim)
 ax[3,1].set_ylim(mag_lim)
 ax[3,2].set_ylim(mag_lim)
 
-plt.subplots_adjust(left=0.1, bottom=0.1)
+plt.subplots_adjust(left=0.1, bottom=0.1, right=0.95)
 plt.savefig('splashback_data/flamingo/plots/cornerplot.png')
 plt.show()
 
